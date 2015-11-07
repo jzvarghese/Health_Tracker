fef2113e02b14c69e1a64abbb2f987b878eb2fcb
Justin Varghese <justinzvarghese@gmail.com>
2015-11-07 16:16:45 -0700

    Feat: Add FoodList, a collection of all the food items that the user has consumed

---
+++ js/collections/FoodList.js
@@ -0,0 +1,19 @@
+// js/collections/FoodList.js
+
+var app = app || {};
+
+// FoodList Collection
+// ---------------
+// is a collection of Foods that have been
+// consumed at some point
+
+app.FoodList = Backbone.Collection.extend({
+
+  // Reference to this collection's model.
+  model: app.Food,
+
+
+});
+
+// create an instance of the Days list
+app.foodList = new app.FoodList();
