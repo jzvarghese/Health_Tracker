6b3de9ce13a78a5ca775aaad01f0fa8438f466b2
Justin Varghese <justinzvarghese@gmail.com>
2015-11-13 21:00:20 -0700

    feat: Switch to Nutrionix v1.1 from v2 beta since the beta does not allow jsonp requests for the auto-complete feature

--- js/views/SearchView.js
+++ js/views/SearchView.js
@@ -14,14 +14,15 @@ app.SearchView = Backbone.View.extend({
 
   // The DOM events specific to an item.
   events: {
-    'input': 'autoComplete'
+    'change': 'autoComplete'
   },
 
-  // base URL for Nutrionix v2 beta
-  baseURL: 'https://apibeta.nutritionix.com',
-
-  // auto-complete end point
-  autoCompleteEndPoint: '/v2/autocomplete?q=',
+  // base URL for Nutrionix v1.1
+  searchEndPoint: 'https://api.nutritionix.com/v1_1/search/',
+  searchFields: '?results=0%3A10&cal_min=0&cal_max=50000&fields=item_name%2Cb' +
+    'rand_name%2Citem_id%2Cbrand_id%2Cnf_calories%2Cnf_protein%2Cnf_total_car' +
+    'bohydrate%2Cnf_dietary_fiber%2Cnf_vitamin_a_dv%2Cnf_vitamin_c_dv%2Cnf_cal'+
+    'cium_dv%2Cnf_iron_dv',
 
 
 
@@ -36,26 +37,40 @@ app.SearchView = Backbone.View.extend({
   // this function will send off a GET request to the Nutrionix v2 beta
   // API for list of possible search terms
   autoComplete: function() {
-    console.log('changed');
-    console.log(this.$el.val());
+    var self = this;
+    //console.log('changed');
+    //console.log(this.$el.val());
 
+    // get the value and make sure it's nonzero
+    var query = this.$el.val().trim();
+
+    if(query.length === 0) {
+      console.log('no search');
+      return 0;
+    }
     // note .trim() removes the whitespace at the beginning and end
     // of the string
-    var autoCompleteQuery = baseURL + autoCompleteEndPoint +
-      encodeURIComponent(this.$el.val().trim()) + app.appID + app.appKey;
+    var queryURL = this.searchEndPoint + encodeURIComponent(query) +
+      this.searchFields + app.appID + app.appKey;
 
     // get the auto complete terms
     $.ajax({
-
-
+      url: queryURL,
+      dataType: 'json'
     })
-    .done(function() {
-        alert( "success" );
+    .done(function(data) {
+      // populate the auto complete list with the data
+      //data.forEach(self.renderGuesses);
+      console.log('success');
     })
     .fail(function() {
-        alert( "error" );
+      console.log(error);
     });
 
+  },
+
+  renderGuesses: function (el, index, array) {
+    console.log(el.text);
   },
 
   // Re-render the titles of the todo item.
